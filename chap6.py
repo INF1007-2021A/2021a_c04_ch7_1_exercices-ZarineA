88743fd523a1c59d9cea9006752267b665b88c07
ZarineA <zarineardekani@gmail.com>
2021-11-08 18:40:09 -0500

    Exercices complétés

---
+++ chap6.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def order(values: list = None) -> list:
+    if values is None:
+        values = [input("Give a value: ") for _ in range(10)]
+
+    return sorted(values)  # We assume values are heterogeneous
+
+
+def anagrams(words: list = None) -> bool:
+    if words is None:
+        words = [input("Enter a word: ") for i in range(2)]
+
+    return sorted(words[0]) == sorted(words[1])
+
+
+def contains_doubles(items: list) -> bool:
+    return len(items) != len(set(items))
+
+
+def best_grades(student_grades: dict) -> dict:
+    # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
+    best_student = sorted(student_grades.items(), key=lambda grade: sum(grade[1]) / len(grade[1]), reverse=True)[0]
+
+    return {best_student[0]: sum(best_student[1]) / len(best_student[1])}
+
+
+def frequence(sentence: str) -> dict:
+    # TODO: Afficher les lettres les plus fréquentes
+    #       Retourner le tableau de lettres
+
+    frequencies = {}
+    for letter in sentence:
+        if letter not in frequencies:
+            frequencies[letter] = 1
+        else:
+            frequencies[letter] += 1
+
+    return frequencies
+
+
+def get_recipes():
+    # TODO: Demander le nom d'une recette, puis ses ingredients et enregistrer dans une structure de données
+    recipe_name = input("Enter a recipe name: ")
+    ingredients = []
+
+    ingredient = input("Give an ingredient: ")
+    while ingredient:
+        ingredients.append(ingredient)
+        ingredient = input("Give an ingredient: ")
+
+    return {recipe_name: ingredients}
+
+
+def print_recipe(ingredients: dict) -> None:
+    # TODO: Demander le nom d'une recette, puis l'afficher si elle existe
+
+    print(ingredients.get(input("What recipe do you want to consult? "), ""))
+
+
+def main() -> None:
+    print(f"On essaie d'ordonner les valeurs...")
+    order()
+
+    print(f"On vérifie les anagrammes...")
+    anagrams()
+
+    my_list = [3, 3, 5, 6, 1, 1]
+    print(f"Ma liste contient-elle des doublons? {contains_doubles(my_list)}")
+
+    grades = {"Bob": [90, 65, 20], "Alice": [85, 75, 83]}
+    best_student = best_grades(grades)
+    print(f"{list(best_student.keys())[0]} a la meilleure moyenne: {list(best_student.values())[0]}")
+
+    sentence = "bonjour, je suis une phrase. je suis compose de beaucoup de lettre. oui oui"
+    frequence(sentence)
+
+    print("On enregistre les recettes...")
+    recipes = get_recipes()
+
+    print("On affiche une recette au choix...")
+    print_recipe(recipes)
+
+
+if __name__ == '__main__':
+    main()
--- exercice.py
+++ exercice.py
@@ -2,13 +2,83 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
-
-
+import math
+import chap6
+import turtle
 
 # TODO: Définissez vos fonction ici
+
+
+def volume_masse_ellipsoide(a = 1, b = 1, c = 1, masse_volumique = 1):
+    volume = 4 / 3 * math.pi * a * b * c
+    return volume, volume * masse_volumique
+
+
+def dessiner_arbre(nombre_branches, longueur_branche, epaisseur, angle):
+    rapport_taille_branches = 4/5
+    turtle.pensize(epaisseur)
+    turtle.forward(longueur_branche)
+    if nombre_branches > 1:
+        longueur_prochaine_branche = longueur_branche * rapport_taille_branches
+        prochaine_epaisseur = epaisseur * rapport_taille_branches
+        prochain_angle = angle * rapport_taille_branches
+        turtle.left(angle)
+        dessiner_arbre(nombre_branches - 1, longueur_prochaine_branche, prochaine_epaisseur, prochain_angle)
+        turtle.right(angle*2)
+        dessiner_arbre(nombre_branches - 1, longueur_prochaine_branche, prochaine_epaisseur, prochain_angle)
+        turtle.left(angle)
+    turtle.backward(longueur_branche)
+    turtle.pensize(epaisseur / rapport_taille_branches)
+
+
+def valide(adn):
+    somme_car_valides = adn.count('a') +  adn.count('t') + adn.count('g') + adn.count('c')
+    return len(adn) != 0 and somme_car_valides == len(adn)
+
+
+def saisie(texte):
+    saisie = input(texte)
+    while not valide(saisie):
+        print("Saisie invalide")
+        saisie = input(texte)
+    return saisie
+
+
+def proportion(chaine, sequence):
+    dans_chaine = 0
+    for car in sequence:
+        if car in chaine:
+            dans_chaine += 1
+    return dans_chaine / len(sequence)
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
 
-    pass
+    # 1)
+    a = int(input("a = "))
+    b = int(input("b = "))
+    c = int(input("c = "))
+    mass_volumique = int(input("Masse volumique = "))
+    volume, masse = volume_masse_ellipsoide(a, b, c, mass_volumique)
+    print("Volume de l'ellipsoïde :", volume)
+    print("Masse de l'ellipsoïde :", masse)
+
+    # 2)
+    print("Entrez une phrase :")
+    phrase = input()
+    dict = chap6.frequence(phrase)
+    tri = lambda dictionnaire: sorted(dictionnaire, key=dictionnaire.__getitem__)
+    print("Lettre la plus fréquente :", tri(dict)[-1])
+
+    # 3)
+    turtle.color("green")
+    turtle.left(90)
+    dessiner_arbre(6, 50, 5, 35)
+    turtle.done()
+
+    # 4)
+    sequence_ADN = saisie("Séquence d'ADN : ")
+    chaine_ADN = saisie("Chaîne d'ADN : ")
+    print("Proportion de sequence dans la chaine :", proportion(chaine_ADN, sequence_ADN))
+
